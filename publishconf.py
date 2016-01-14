62034403bf472845e91a4e5d04b48d00e06b8b7b
Thomas Graf <git@thomasgraf.net>
2016-01-14 01:23:00 -0500

    adapt for github url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://mathling.thomasgraf.net'
+SITEURL = 'http://complab-stonybrook.github.io/mlrg'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
